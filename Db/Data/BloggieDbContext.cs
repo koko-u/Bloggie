a7e2a4f970b328c67cb947f3997a239f316317d2
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2024-05-04 23:07:02 +0900

    dotnet ef migrations add Initial -p Db -s Web (again)

--- Db/Data/BloggieDbContext.cs
+++ Db/Data/BloggieDbContext.cs
@@ -7,4 +7,18 @@ public class BloggieDbContext(DbContextOptions<BloggieDbContext> opt) : DbContex
 {
     public DbSet<BlogPost> BlogPosts => Set<BlogPost>();
     public DbSet<Tag> Tags => Set<Tag>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BlogPost>()
+            .Property(b => b.Id)
+            .HasDefaultValueSql("NEWID()");
+        modelBuilder.Entity<BlogPost>()
+            .Property(b => b.IsVisible)
+            .HasDefaultValue(false);
+
+        modelBuilder.Entity<Tag>()
+            .Property(t => t.Id)
+            .HasDefaultValueSql("NEWID()");
+    }
 }
