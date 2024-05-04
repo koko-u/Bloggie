08a12fbc3aabed373de7f2292392b6957a1c98e2
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2024-05-05 03:07:55 +0900

    setup data binding for page

---
+++ Web/Models/ViewModels/AddBlogPost.cs
@@ -0,0 +1,23 @@
+namespace Bloggie.Web.Models.ViewModels;
+
+public class AddBlogPost
+{
+    public string? Heading { get; set; }
+
+    public string? PageTitle { get; set; }
+
+    public string? Content { get; set; }
+
+    public string? ShortDescription { get; set; }
+
+    public string? FeaturedImageUrl { get; set; }
+
+    public string? UrlHandle { get; set; }
+
+    public DateTime? PublishedOn { get; set; }
+
+    public string? Author { get; set; }
+
+    public bool IsVisible { get; set; } = false;
+
+}
