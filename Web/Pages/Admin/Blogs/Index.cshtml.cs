39735947487af96610b2fc495e78b6acb24bd372
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2024-05-05 10:52:23 +0900

    delete blog post

--- Web/Pages/Admin/Blogs/Index.cshtml.cs
+++ Web/Pages/Admin/Blogs/Index.cshtml.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Bloggie.Db.Data;
 using Bloggie.Db.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,5 +16,19 @@ public class IndexModel(BloggieDbContext dbContext, IMapper mapper) : PageModel
     {
         var blogPosts = await dbContext.BlogPosts.ToListAsync();
         BlogPosts = mapper.Map<List<BlogPost>, List<BlogPostRow>>(blogPosts);
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(Guid id)
+    {
+        var dbBlogPost = await dbContext.BlogPosts.SingleOrDefaultAsync(blogPost => blogPost.Id == id);
+        if (dbBlogPost is null)
+        {
+            return RedirectToPage("/Admin/Blogs/Index");
+        }
+
+        dbContext.BlogPosts.Remove(dbBlogPost);
+        await dbContext.SaveChangesAsync();
+
+        return RedirectToPage("/Admin/Blogs/Index");
     }
 }
